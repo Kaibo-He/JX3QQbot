971f62d2e165eab34f204625a7e9aceeacbc06ad
Kaibo-He <kaibo.he@mail.utoronto.ca>
2025-04-19 20:49:55 -0400

    Update achievement_handler.py

---
+++ handlers/achievement_handler.py
@@ -0,0 +1,27 @@
+def handle_role_achievement(content: str) -> str:
+    parts = content.strip().split()
+
+    if len(parts) > 2:
+        return {
+            "content": "格式错误，如需查询开服状态请输入：\n开服 [区服]",
+            "file_image": None
+        }
+    server = parts[1] if len(parts) == 2 else DEFAULT_SERVER
+        
+    status = 0
+    if not status:
+        return {
+            "content": f"无法获取【{server}】的开服状态，接口可能出错或区服名称不正确。",
+            "file_image": None
+        }
+        
+    msg = (
+        f"【{status['server']}】（{status['zone']}）\n"
+        f"状态：{status['status']}\n"
+        f"最后更新时间：{status['update_time']}"
+    )
+    
+    return {
+        "content": msg,
+        "file_image": None
+    }
