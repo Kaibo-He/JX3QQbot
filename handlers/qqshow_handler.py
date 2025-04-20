64ac697d8def3d3ed8d4ed78e275a7a14d2caf4f
Kaibo-He <kaibo.he@mail.utoronto.ca>
2025-04-19 20:33:22 -0400

    Update qqshow_handler.py

--- handlers/qqshow_handler.py
+++ handlers/qqshow_handler.py
@@ -5,16 +5,9 @@ from api.jx3api import get_role_qqshow
 
 def handle_qqshow_query(content: str) -> str:
     parts = content.strip().split()
-        
-    name = "王熙凤"
-    server = "梦江南"
 
-    if len(parts) > 3:
-        return {
-            "content": "格式错误，如需查询角色名片请输入：\n名片/qq秀/QQ秀 角色id [区服]",
-            "image":None
-        }
-
+    name = parts[1] if len(parts) >= 2 else "王熙凤"
+    server = parts[2] if len(parts) >= 3 else DEFAULT_SERVER
 
     result = get_role_qqshow(server=server, name=name)
     if not result:
@@ -22,9 +15,7 @@ def handle_qqshow_query(content: str) -> str:
             "content": f"查询失败，可能是区服或角色名错误，或接口超时，请稍后重试。",
             "image": None
         }
-    name = parts[1] if len(parts) >= 2 else "王熙凤"
-    server = parts[2] if len(parts) == 3 else DEFAULT_SERVER
-
+    
     # 下载图片内容
     try:
         image_res = requests.get(result["showAvatar"], timeout=5)
